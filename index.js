d72614712bd386f1ce8f920f4e4953e8d0384958
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2018-12-02 17:33:20 +0330

    add blink and breath function

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+// LoadImage Function:
 var images = {};
+
+// ResourceLoaded Function:
+var totalResources = 6;
+var numResourcesLoaded = 0;
+var fps = 30;
+
+// Redraw Function:
+var context = document.getElementById("canvas").getContext("2d");
+var charX = 220;
+var charY = 250;
+var x = charX;
+var y = charY;
+
+// UpdateBreath Function:
+var breathInc = 0.1;
+var breathDir = 1;
+var breathAmt = 0;
+var breathMax = 2;
+var breathInterval = setInterval(updateBreath, 1000 / fps);
+
+// UpdateBlink & Blink Function:
+var maxEyeHeight = 14;
+var curEyeHeight = maxEyeHeight;
+var eyeOpenTime = 0;
+var timeBtwBlinks = 4000;
+var blinkUpdateTime = 200;
+var blinkTimer = setInterval(updateBlink, blinkUpdateTime);
 
 loadImage("leftArm");
 loadImage("legs");
@@ -7,6 +35,7 @@ loadImage("rightArm");
 loadImage("head");
 loadImage("hair");
 
+// Load All Images:
 function loadImage(name) {
   images[name] = new Image();
   images[name].onload = function() {
@@ -15,10 +44,7 @@ function loadImage(name) {
   images[name].src = `images/${name}.png`;
 }
 
-var totalResources = 6;
-var numResourcesLoaded = 0;
-var fps = 30;
-
+// Make Sure All Images Loaded After That Call Redraw Function
 function resourceLoaded() {
   numResourcesLoaded += 1;
   if (numResourcesLoaded === totalResources) {
@@ -26,21 +52,82 @@ function resourceLoaded() {
   }
 }
 
-var context = document.getElementById("canvas").getContext("2d");
-
-var charX = 220;
-var charY = 200;
-
+// Main Fuction:
 function redraw() {
-  var x = charX;
-  var y = charY;
+  canvas.width = canvas.width; // Clears the Canvas
 
-  canvas.width = canvas.width; // clears the canvas
+  drawEllipse(x + 40, y + 29 + breathAmt, 160 - breathAmt, 6); // Shadow
+  // We want the shadow behind all the other image layers sp an ellipse is drawn at the beginning of the redraw function.
 
-  context.drawImage(images["leftArm"], x + 40, y - 42);
+  context.drawImage(images["leftArm"], x + 40, y - 42 - breathAmt);
   context.drawImage(images["legs"], x, y);
   context.drawImage(images["torso"], x, y - 50);
-  context.drawImage(images["rightArm"], x - 15, y - 42);
-  context.drawImage(images["head"], x - 10, y - 125);
-  context.drawImage(images["hair"], x - 37, y - 138);
+  context.drawImage(images["rightArm"], x - 15, y - 42 - breathAmt);
+  context.drawImage(images["head"], x - 10, y - 125 - breathAmt);
+  context.drawImage(images["hair"], x - 37, y - 138 - breathAmt);
+
+  drawEllipse(x + 47, y - 68, 8, curEyeHeight); // Left Eye
+  drawEllipse(x + 58, y - 68, 8, curEyeHeight); // Right Eye
+}
+
+// Eyes Function:
+function drawEllipse(centerX, centerY, width, height) {
+  context.beginPath();
+  context.moveTo(centerX, centerY - breathAmt - height / 2);
+  context.bezierCurveTo(
+    centerX + width / 2,
+    centerY - breathAmt - height / 2,
+    centerX + width / 2,
+    centerY - breathAmt + height / 2,
+    centerX,
+    centerY - breathAmt + height / 2
+  );
+  context.bezierCurveTo(
+    centerX - width / 2,
+    centerY - breathAmt + height / 2,
+    centerX - width / 2,
+    centerY - breathAmt - height / 2,
+    centerX,
+    centerY - breathAmt - height / 2
+  );
+  context.fillStyle = "black";
+  context.fill();
+  context.closePath();
+}
+
+//Blink & UpdateBlink Function
+function updateBlink() {
+  eyeOpenTime += blinkUpdateTime;
+  if (eyeOpenTime >= timeBtwBlinks) {
+    blink();
+  }
+}
+
+function blink() {
+  curEyeHeight -= 1;
+  if (curEyeHeight <= 0) {
+    eyeOpenTime = 0;
+    curEyeHeight = maxEyeHeight;
+  } else {
+    setTimeout(blink, 10);
+  }
+}
+
+// Brath Function:
+// The purpose of this process is to update the variable breathAmt which we will use to represent the constant
+//  breathing of our character in the form of a subtle rise and fall of the head and arms.
+function updateBreath() {
+  if (breathDir === 1) {
+    // Breath In:
+    breathAmt -= breathInc;
+    if (breathAmt < -breathMax) {
+      breathDir = -1;
+    }
+  } else {
+    // Breath Out:
+    breathAmt += breathInc;
+    if (breathAmt > breathMax) {
+      breathDir = 1;
+    }
+  }
 }
